a277c4cef6e00d4837966d3ec499b62ff8bce67b
yarasa27 <elcan.abbasoff27@gmail.com>
2025-07-26 01:44:02 +0400

    finished header Html, Css, JavasCript and starting section Home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,28 @@
-/*=============== SHOW MENU ===============*/
+/* SHOW MENU */
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
 
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
-/*=============== REMOVE MENU MOBILE ===============*/
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
+/* REMOVE MENU MOBILE */
+const nacLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SWIPER HOME ===============*/ 
 
